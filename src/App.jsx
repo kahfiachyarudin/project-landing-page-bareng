f77a441425fa8390ee866f06e5cf00a513f800b5
RafifArdinata <rafif0101@student.abudzar.sch.id>
2026-05-11 13:27:03 +0700

    penambahan function utama

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Hero from './components/hero'
 import './App.css'
 
 function App() {
   return (
     <>
+      <Hero />
       <h1 className="text-5xl text-red-500">
       Tailwind jalan
     </h1>
