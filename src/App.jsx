25a74708c53e9fcc2e710af42d2385bcf93f7394
barakaramadhan <barakaramadhan444@gmail.com>
2026-05-12 10:08:04 +0700

    penambahan & perubahan pada file RoomsInspiration.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Image from '../src/components/image.jsx'
+import RoomsInspiration from '../src/components/RoomsInspiration.jsx'
 import './App.css'
 
 function App() {
   return (
     <>
      <Image />
+     <RoomsInspiration />
     </>
   )
 }
